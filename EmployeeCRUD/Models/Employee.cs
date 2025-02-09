55f20c90cb2fd1735ea7a246e60d249da9e562f5
AliElyas4517 <aliilyas4517@gmail.com>
2025-02-09 17:31:36 +0500

    Add project files.

---
+++ EmployeeCRUD/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Models;
+
+[Index("Email", Name = "UQ__Employee__A9D10534E188DDD5", IsUnique = true)]
+public partial class Employee
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; } = null!;
+
+    [StringLength(100)]
+    public string Email { get; set; } = null!;
+
+    public int? Age { get; set; }
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal Salary { get; set; }
+}
---
+++ EmployeeCRUD/Models/EmployeeCrudContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Models;
+
+public partial class EmployeeCRUDContext : DbContext
+{
+    public EmployeeCRUDContext()
+    {
+    }
+
+    public EmployeeCRUDContext(DbContextOptions<EmployeeCRUDContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07903CFBB2");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
